b2170f8c4377365a4f52a491649fc3144bda6312
Aryan Gupta <aryangupta10112@gmail.com>
2025-03-18 08:41:29 +0530

    ffsas

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -12,24 +12,38 @@ import {
     updateUserAvatar,
     updateUserCoverImage,
 } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { checkUser } from "../middlewares/openAuth.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(registerUser); // Multer moved to app.js
+router.route("/register").post(
+    upload.fields([
+        { name: "avatar", maxCount: 1 },
+        {
+            name: "coverImage",
+            maxCount: 1,
+        },
+    ]),
+    registerUser
+);
 
 router.route("/login").post(loginUser);
 
-// Secured routes
+//secured routes
 router.route("/logout").post(verifyJWT, logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
 router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 
-router.route("/avatar").patch(verifyJWT, updateUserAvatar);
-router.route("/cover-image").patch(verifyJWT, updateUserCoverImage);
+router
+    .route("/avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+router
+    .route("/cover-image")
+    .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
 
 router.route("/c/:username").get(checkUser, getUserChannelProfile);
 router.route("/history").get(verifyJWT, getWatchHistory);
